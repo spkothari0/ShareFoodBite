bc60b8a7c49be72cc030d4c9dca4693261cf3ed5
Shreyas Kothari <kothari.shr@northeastern.edu>
2024-05-09 16:27:08 -0400

    Update Firebase API key in api.service.firbase.js

--- sharebite-application/AppUI/sharebite/src/services/api.service.firbase.js
+++ sharebite-application/AppUI/sharebite/src/services/api.service.firbase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 export const locationKeyConstant = import.meta.env.VITE_LOCATION_KEY_CONSTANT;
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC5UYieIuXz6ZZAUUWzhdsJGBx515o_uN4",
-//   authDomain: "sharebite-79c85.firebaseapp.com",
-//   projectId: "sharebite-79c85",
-//   storageBucket: "sharebite-79c85.appspot.com",
-//   messagingSenderId: "453512320390",
-//   appId: "1:453512320390:web:99e471b79bee672dfc4807"
-// };
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
