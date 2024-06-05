6c43ba000cfb7e2928178a761f67cfa2779f17e5
Shreyas Kothari <spkothari0@gmail.com>
2024-06-04 22:15:02 -0400

    enabling pwa

--- sharebite-application/AppUI/vite.config.ts
+++ sharebite-application/AppUI/vite.config.ts
@@ -12,102 +12,101 @@ export default defineConfig({
   // base: '/ShareFoodBite/',
   //List of plugins used in the app
   plugins: [react(),
-  //   VitePWA({
-  //     registerType: 'autoUpdate',
-  //     injectRegister: 'auto',
-  //     strategies:'generateSW',
-  //     devOptions: {
-  //       enabled: true,
-  //     },
-  //     //PWA configuration
-  //     manifest: {
-  //       "name": "ShareBite",
-  //       "short_name": "CourseRegist",
-  //       "start_url": "./",
-  //       "display": "standalone",
-  //       "background_color": "#fff",
-  //       "description": "Food Sharing and Donation App",
-  //       "theme_color": "#ffffff",
-  //       "icons": [
-  //         {
-  //           "src": "images/pwa-64x64.png",
-  //           "sizes": "64x64",
-  //           "type": "image/png"
-  //         },
-  //         {
-  //           "src": "images/pwa-192x192.png",
-  //           "sizes": "192x192",
-  //           "type": "image/png"
-  //         },
-  //         {
-  //           "src": "images/pwa-512x512.png",
-  //           "sizes": "512x512",
-  //           "type": "image/png"
-  //         },
-  //         {
-  //           "src": "images/maskable-icon-512x512.png",
-  //           "sizes": "512x512",
-  //           "type": "image/png",
-  //           "purpose": "maskable"
-  //         }
-  //       ]
-  //     },
-  //     //Workbox configuration
-  //     workbox:{
-  //       runtimeCaching:[
-  //         {
-  //           urlPattern: ({ url }) => {
-  //             return url.pathname.includes('/')
-  //           },
-  //           handler: 'NetworkFirst',
-  //           method: 'GET',
-  //           options: {
-  //             cacheName: 'post-data-cache',
-  //             expiration: {
-  //               maxEntries: 10,
-  //               maxAgeSeconds: 60 * 60 * 24
-  //             },
-  //             cacheableResponse: {
-  //               statuses: [0, 200]
-  //             },
-  //           }
-  //         },
-  //         {
-  //           urlPattern: ({ url }) => {
-  //             return url.pathname.includes('/')
-  //           },
-  //           handler: 'NetworkFirst',
-  //           method: 'GET',
-  //           options: {
-  //             cacheName: 'post-data-cache',
-  //             expiration: {
-  //               maxEntries: 10,
-  //               maxAgeSeconds: 60 * 60 * 24
-  //             },
-  //             cacheableResponse: {
-  //               statuses: [0, 200]
-  //             },
-  //           }
-  //         },
-  //         {
-  //           urlPattern: ({ url }) => {
-  //             return url.pathname.includes('getALlEventData')
-  //           },
-  //           handler: 'NetworkFirst',
-  //           method: 'GET',
-  //           options: {
-  //             cacheName: 'event-data-cache',
-  //             expiration: {
-  //               maxEntries: 10,
-  //               maxAgeSeconds: 60 * 60 * 24
-  //             },
-  //             cacheableResponse: {
-  //               statuses: [0, 200]
-  //             },
-  //           }
-  //         }
-  //       ]
-  //     }
-  // })
-],
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      strategies:'generateSW',
+      devOptions: {
+        enabled: true,
+      },
+      //PWA configuration
+      manifest: {
+        "name": "ShareBite",
+        "short_name": "CourseRegist",
+        "start_url": "./",
+        "display": "standalone",
+        "background_color": "#fff",
+        "description": "Food Sharing and Donation App",
+        "theme_color": "#ffffff",
+        "icons": [
+          {
+            "src": "images/pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "images/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "images/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "images/maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      },
+      //Workbox configuration
+      workbox:{
+        runtimeCaching:[
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.includes('/')
+            },
+            handler: 'NetworkFirst',
+            method: 'GET',
+            options: {
+              cacheName: 'post-data-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+            }
+          },
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.includes('/')
+            },
+            handler: 'NetworkFirst',
+            method: 'GET',
+            options: {
+              cacheName: 'post-data-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+            }
+          },
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.includes('getAllEventData')
+            },
+            handler: 'NetworkFirst',
+            method: 'GET',
+            options: {
+              cacheName: 'event-data-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+            }
+          }
+        ]
+      }
+  })],
 })
